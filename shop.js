60bbd763760040e9337eb4332ec276a4c67e6240
ceciliasegura <ceciliasegura.t@hotmail.com>
2023-01-15 16:43:53 +0100

    Add code

--- shop.js
+++ shop.js
@@ -1,14 +1,3 @@
-const productsCoffeShop = [
-    { id: 1, price: 9, name: 'Costa Rica Tarrazú', description: 'Paquete de café, 250 gr', img: 'assets/cafeverde.png' },
-    { id: 2, price: 9, name: 'Colombia Los Naranjos', description: 'Paquete de café, 250 gr', img: 'assets/caferojo.png' },
-    { id: 3, price: 9, name: 'Laos Amanecer', description: 'Paquete de café, 250 gr', img: 'assets/cafeverdeclaro.png' },
-    { id: 4, price: 9, name: 'Etiopía Yrgacheff', description: 'Paquete de café, 250 gr', img: 'assets/cafemorado.png' },
-    { id: 5, price: 15, name: 'Kenia Ndunduri ', description: 'Paquete de café, 250 gr', img: 'assets/Coffee bag kenia shop.png' },
-    { id: 6, price: 17, name: 'Etiopía Sidamo', description: 'Paquete de café, 250 gr', img: 'assets/coffe bag etipia shop.png' },
-    { id: 7, price: 12, name: 'Costa Rica Monte Bello', description: 'Paquete de café, 250 gr', img: 'assets/coffe back costarica shop.png' },
-    { id: 8, price: 9, name: 'Colombia La Casita', description: 'Paquete de café, 250 gr', img: 'assets/ coffe shop bag colombia shop.jpg' }
-]
-
 const coffes = document.querySelectorAll(".cafe3");
 const addBtnCoffes = document.querySelectorAll(".button_section3");
 const shoppingBagStore = JSON.parse(localStorage.getItem("bag"));
